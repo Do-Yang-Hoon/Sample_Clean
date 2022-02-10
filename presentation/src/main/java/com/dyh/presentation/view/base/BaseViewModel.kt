61e020670643950dd53d081350d3dafbbe1f5a52
dyh <hoon77do@rsquare.co.kr>
2022-02-10 18:46:17 +0900

    test

--- presentation/src/main/java/com/dyh/presentation/view/base/BaseViewModel.kt
+++ presentation/src/main/java/com/dyh/presentation/view/base/BaseViewModel.kt
@@ -11,41 +11,10 @@ import kotlinx.coroutines.Dispatchers
 
 abstract class BaseViewModel : ViewModel() {
 
-    val disposable = CompositeDisposable()
-
-    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        setLoading(false)
-        setExceptionError(throwable.message)
-
-        if (BuildConfig.DEBUG) {
-            throwable.printStackTrace()
-        }
-    }
-
-    protected val ioDispatchers = Dispatchers.IO + coroutineExceptionHandler
-    protected val mainDispatchers = Dispatchers.Main + coroutineExceptionHandler
-
     // Loading
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean>
         get() = _isLoading
-
-    // Exception Error
-    private val _exceptionError = MutableLiveData<Event<String?>>()
-    val exceptionError: LiveData<Event<String?>>
-        get() = _exceptionError
-
-
-
-    override fun onCleared() {
-        disposable.dispose()
-        //destroyInstance()
-        super.onCleared()
-    }
-
-
-
-    private fun setExceptionError(errorMsg: String? = null) = _exceptionError.postValue(Event(errorMsg))
 
     fun setLoading(isLoading: Boolean) = _isLoading.postValue(isLoading)
 }
